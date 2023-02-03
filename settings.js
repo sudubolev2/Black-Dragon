a2bc8880984011728da16d751659140bb138b3d2
sudubolev2 <124289996+sudubolev2@users.noreply.github.com>
2023-02-03 11:48:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
+global.owner = ['94757530317'] //ur owner number
+global.ownername = "Hiruwa Official" //ur owner name
+global.ytname = "YT: MR.HIRUWA MODZ" //ur yt chanel name
+global.socialm = "ꪶMr.Hiruwa Modz Ofcꫂ™" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
+global.ownernomer = "94757530317" //ur number
+global.premium = ['94757530317'] //ur premium number
+global.botname = 'Kᴵᴺᴳ🧸ＡＰＥＸ🕊️〽️' //ur bot name
+global.linkz = "https://chat.whatsapp.com/LAKRMUpKrr25heG3JrgKbn" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/@cyberxhiruwa" //ur website to be displayed
 global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
 global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.packname = "Sticker By Kᴵᴺᴳ🧸ＡＰＥＸ🕊️〽️" //ur sticker watermark packname
+global.author = "Kᴵᴺᴳ🧸ＡＰＥＸ🕊️〽️" //ur sticker watermark author
+global.wm = "Kᴵᴺᴳ🧸ＡＰＥＸ🕊️〽️ 𝗜𝗡𝗖" //ur watermark
 
 // Other
 global.sessionName = 'session'
